f8950a2a2bf8402f16eb9c496d60f3cded655ecd
Garrett Haines <70733202+GarrettHaines@users.noreply.github.com>
2022-07-28 12:03:03 -0400

    Update OneAgentAutodeploy.ps1

--- OneAgentAutodeploy.ps1
+++ OneAgentAutodeploy.ps1
@@ -20,7 +20,7 @@ function install {
 	# Download Dynatrace OneAgent installer for Windows
 	Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;"
 								Invoke-WebRequest -Uri "$($tenantID)/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default"
-										  -Headers @{ 'Authorization' = "Api-Token $($token)" }
+										  -Headers @{ "Authorization" = "Api-Token $($token)" }
 										  -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'
 
 	# Run file and begin installation
