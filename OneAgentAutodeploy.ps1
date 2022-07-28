02108236ff7b2ba67c906c8954ce6cac30e640e5
Garrett Haines <70733202+GarrettHaines@users.noreply.github.com>
2022-07-28 11:07:04 -0400

    Update OneAgentAutodeploy.ps1

--- OneAgentAutodeploy.ps1
+++ OneAgentAutodeploy.ps1
@@ -1,6 +1,9 @@
 # Dynatrace OneAgent Autodeploy
 # Garrett Haines (Sprint)
 
+Add-Type -AssemblyName 'PresentationFramework'
+
+
 # YOUR host group
 $hostGroup = "Autodeploy"
 
@@ -11,16 +14,37 @@ $tenantID = "dkz99748"
 $token = "dt0c01.ZLABM3EQTSL7CRIFVTOM6667.WZHKGFVVF6IEXAQLI7GM4QUMWNFX2RKHSEWCRE6RGWASCI2A7ZFQC5RHK7ON3DAQ"
 
 
+$alreadyInstalled = Get-WmiObject win32_product -filter "Name like 'Dynatrace OneAgent'"
 
-# Download Dynatrace OneAgent installer for Windows
-Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-							Invoke-WebRequest -Uri 'https://$($tenantID).sprint.dynatracelabs.com/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default'
-											  -Headers @{ 'Authorization' = 'Api-Token $($token)' }
-											  -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'"
+if ($alreadyInstalled) {
+	$continue = "Yes"
+	$continue = [System.Windows.MessageBox]::Show(", $caption, 'YesNo');
+	
+	$caption = "*** IT APPEARS THIS IS A FACTORY KEY ***"    
+   	$message = "Are you Sure You Want To Proceed:"
+	 
+	if ($continue -eq 'Yes') {
+		install
+	}
+	else {
+		echo "Installation aborted."
+	}
+} 
+else {
+	install
+}
 
-# Run file and begin installation
-Invoke-Expression -Command ".\Dynatrace-OneAgent-Installer-Windows.exe
-								--set-infra-only=false
-								--set-app-log-content-access=true
-								--set-host-group=$($hostGroup)
-								--quiet"
+function install {
+	# Download Dynatrace OneAgent installer for Windows
+	Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+								Invoke-WebRequest -Uri 'https://$($tenantID).sprint.dynatracelabs.com/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default'
+										  -Headers @{ 'Authorization' = 'Api-Token $($token)' }
+										  -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'"
+
+	# Run file and begin installation
+	Invoke-Expression -Command ".\Dynatrace-OneAgent-Installer-Windows.exe
+					--set-infra-only=false
+					--set-app-log-content-access=true
+					--set-host-group=$($hostGroup)
+					--quiet"
+}
