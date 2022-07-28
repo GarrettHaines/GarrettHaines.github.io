6a846f3d060231fcaf30c3c9323071fc2fb27091
Garrett Haines <70733202+GarrettHaines@users.noreply.github.com>
2022-07-28 12:39:23 -0400

    Update OneAgentAutodeploy.ps1

--- OneAgentAutodeploy.ps1
+++ OneAgentAutodeploy.ps1
@@ -17,14 +17,17 @@ $token = "dt0c01.ZLABM3EQTSL7CRIFVTOM6667.WZHKGFVVF6IEXAQLI7GM4QUMWNFX2RKHSEWCRE
 # Deploy Dynatrace's OneAgent
 function deploy {
 	# Download Dynatrace OneAgent installer for Windows
+	echo "Downloading Windows OneAgent installer..."
 	Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '$($tenantURL)/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default' -Headers @{ 'Authorization' = 'Api-Token $($token)' } -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'"
-
+	
 	# Run file and begin installation
+	echo "Installing OneAgent..."
 	Invoke-Expression -Command ".\Dynatrace-OneAgent-Installer-Windows.exe --set-infra-only=false --set-app-log-content-access=true --set-host-group=$($hostGroup) --quiet"
+	echo "Installation complete."
 }
 
-Add-Type -AssemblyName "PresentationFramework"
 
+Add-Type -AssemblyName "PresentationFramework"
 $alreadyInstalled = Get-WmiObject win32_product -filter "Name like 'Dynatrace OneAgent'"
 
 if ($alreadyInstalled) {
@@ -34,10 +37,12 @@ if ($alreadyInstalled) {
 	$continue = "No"
 	$continue = [System.Windows.MessageBox]::Show($message, $caption, 'YesNo');
 	 
-	if ($continue -eq 'Yes')
+	if ($continue -eq 'Yes') {
 		deploy
-	else
+	}
+	else {
 		echo "Installation aborted."
+	}
 } 
 else {
 	deploy
