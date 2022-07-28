8897a2b1eff41873735e50b3329ac30e454f5ae8
Garrett Haines <70733202+GarrettHaines@users.noreply.github.com>
2022-07-28 12:33:29 -0400

    Update OneAgentAutodeploy.ps1

--- OneAgentAutodeploy.ps1
+++ OneAgentAutodeploy.ps1
@@ -17,7 +17,7 @@ $token = "dt0c01.ZLABM3EQTSL7CRIFVTOM6667.WZHKGFVVF6IEXAQLI7GM4QUMWNFX2RKHSEWCRE
 # Deploy Dynatrace's OneAgent
 function deploy {
 	# Download Dynatrace OneAgent installer for Windows
-	Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '$($tenantID)/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default' -Headers @{ 'Authorization' = 'Api-Token $($token)' } -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'"
+	Invoke-Expression -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '$($tenantURL)/api/v1/deployment/installer/agent/windows/default/latest?arch=x86&flavor=default' -Headers @{ 'Authorization' = 'Api-Token $($token)' } -OutFile 'Dynatrace-OneAgent-Installer-Windows.exe'"
 
 	# Run file and begin installation
 	Invoke-Expression -Command ".\Dynatrace-OneAgent-Installer-Windows.exe --set-infra-only=false --set-app-log-content-access=true --set-host-group=$($hostGroup) --quiet"
